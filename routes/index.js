2ef579e9ecf6bd94ddc9fdcb8d58bddb7ffb2dfe
Cody Norman <wcodynorman@gmail.com>
2014-08-05 14:09:43 -0700

    first commit

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+module.exports = function Route(app){
+	app.get('/', function(req, res){
+		res.render('index', {title: 'Survey Page'});
+	});
+
+	app.post('/process', function(req, res){
+		console.log(req.body);
+		res.render('result', {
+			title: 'Submitted Information',
+			name: req.body.name,
+			location: req.body.location,
+			language: req.body.language,
+			comment: req.body.comment
+		});
+	})
+
+
+
+
+}
